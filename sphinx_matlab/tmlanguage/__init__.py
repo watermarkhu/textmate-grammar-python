da6b4bc20698f53740e468d8e38f3429495a1e05
Mark Shui Hu <watermarkhu@gmail.com>
2023-08-21 21:20:05 +0200

    create json using submodule

--- sphinx_matlab/tmlanguage/__init__.py
+++ sphinx_matlab/tmlanguage/__init__.py
@@ -3,12 +3,17 @@ import plistlib
 import json
 
 
-with open(Path(__file__).parent / "MATLAB.tmLanguage", "rb") as file:
-    TMLIST = plistlib.load(file, fmt=plistlib.FMT_XML)
+tmLanguageFile = Path(__file__).parents[2] / "syntaxes" / "Matlab.tmbundle" / "Syntaxes" / "MATLAB.tmLanguage"
+tmLanguageJSON = Path(__file__).parent / "MATLAB.tmLanguage.json"
 
 
-def write_json(path: str = ""):
-    if not path:
-        path = Path(__file__).parent / "MATLAB.json"
-    with open(path, 'w') as f:
+if tmLanguageFile.exists():
+    with open(tmLanguageFile, "rb") as file:
+        TMLIST = plistlib.load(file, fmt=plistlib.FMT_XML)
+    with open(tmLanguageJSON, 'w') as f:
         f.write(json.dumps(TMLIST, indent=2))
+else:
+    with open(tmLanguageJSON, "r") as file:
+        TMLIST = json.loads(file.read())
+
+
