96462251504dbcd406dcf2ea5af684cb8bfaf155
Mark Hu <watermarkhu@gmail.com>
2024-03-06 23:48:22 +0000

    bump version

--- docs/conf.py
+++ docs/conf.py
@@ -3,7 +3,7 @@ from datetime import date
 # -- Project information -----------------------------------------------------
 
 project = "Texmate Grammar Python"
-version = "0.2.0"
+version = "0.3.0"
 copyright = f"{date.today().year}, Mark Shui Hu"
 author = "Mark Shui Hu"
 
